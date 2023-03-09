2f3c4af8e69b6c36351dc9d58c739f091bf45713
zuzka <zuzkafurtak@gmail.com>
2023-03-09 19:42:36 +0100

    Init

---
+++ src/main/java/com/catan/CatanApplication.java
@@ -0,0 +1,13 @@
+package com.catan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/catan/CatanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
