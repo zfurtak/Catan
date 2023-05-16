528e63cbc67312444bfde032b6cd5db5a6e25fa3
passer085 <117078585+passer085@users.noreply.github.com>
2023-05-16 17:44:59 +0200

    Added usernameTooShort and updated Tests
    Error managment of trying to register a username with less than 4 characters.
    Updated UserTests and created PlayerTests classes

--- backend/src/main/java/com/catan/exceptions/UsernameTooShortException.java
+++ backend/src/main/java/com/catan/exceptions/UsernameTooShortException.java
@@ -4,6 +4,10 @@ public class UsernameTooShortException extends RuntimeException{
     private static final String AUTH_EXCEPTION_ERROR_CODE = "400";
 
     public UsernameTooShortException(String message) {
-        super(message, new Throwable(AUTH_EXCEPTION_ERROR_CODE));
+        new UsernameTooShortException(message, new Throwable(AUTH_EXCEPTION_ERROR_CODE));
+    }
+
+    public UsernameTooShortException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
