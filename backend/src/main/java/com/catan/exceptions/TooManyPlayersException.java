3b7d930912ce0110b839b0b204ca3e052c5e8f30
Emilio Rodrigo Carreira Villalta <emiliorodrigo.ecr@gmail.com>
2023-05-17 10:53:48 +0200

    Exception Classes fixed

--- backend/src/main/java/com/catan/exceptions/TooManyPlayersException.java
+++ backend/src/main/java/com/catan/exceptions/TooManyPlayersException.java
@@ -4,10 +4,6 @@ public class TooManyPlayersException extends RuntimeException{
     private static final String AUTH_EXCEPTION_ERROR_CODE = "403";
 
     public TooManyPlayersException(String message) {
-        new TooManyPlayersException(message, new Throwable(AUTH_EXCEPTION_ERROR_CODE));
-    }
-
-    public TooManyPlayersException(String message, Throwable cause) {
-        super(message, cause);
+        super(message, new Throwable(AUTH_EXCEPTION_ERROR_CODE));
     }
 }
