c87d2c901e533c11f9f20ce67a5cec5ace629ed5
Emilio Rodrigo Carreira Villalta <emiliorodrigo.ecr@gmail.com>
2023-06-05 20:57:24 +0200

    updateVictoryPoints functionality Implemented

--- backend/src/main/java/com/catan/repository/EdgeRepository.java
+++ backend/src/main/java/com/catan/repository/EdgeRepository.java
@@ -1,12 +1,16 @@
 package com.catan.repository;
 
 
+import com.catan.model.Color;
 import com.catan.model.board.Edge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EdgeRepository extends JpaRepository<Edge, Integer> {
 
     Optional<Edge> findById(int id);
+
+    List<Edge> findAllByColorOfEdge(Color color);
 }
